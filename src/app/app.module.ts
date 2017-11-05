5f59c9049aa882d6286f3071e8939ddf0f54c190
David <debeltranc@unal.edu.co>
2017-11-05 21:21:04 +0000

    Feat: using Mozilla Holy-Grail Layout (https://tburleson-layouts-demos.firebaseapp.com/#/stackoverflow)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule.forRoot()
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
